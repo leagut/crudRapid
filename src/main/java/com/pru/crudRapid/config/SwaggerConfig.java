577a1b490c335bbf8483cdf8874e7bf205283801
leandro <leaguts21@gmail.com>
2023-07-09 14:01:51 -0500

    swagger ok

---
+++ src/main/java/com/pru/crudRapid/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.pru.crudRapid.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiDetails())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+
+    private ApiInfo apiDetails(){
+        return new ApiInfo("Springbooot api","library docs","1.0","www.google.com",
+                new Contact("leo","www.google.com","correo@gm.com"),
+                "mit","www.google.com", Collections.emptyList());
+    }
+
+}
